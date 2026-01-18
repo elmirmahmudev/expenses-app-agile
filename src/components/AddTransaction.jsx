fedb9966cf15eefb44b6701e020e0cc92cfcbadd
Elmir Mahmudov <elmirmahmudev@gmail.com>
2026-01-18 23:23:15 +0100

    components added

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTransaction = () => {
+  return <div>{/* AddTransaction content goes here */}</div>;
+};
+
+export default AddTransaction;
---
+++ src/components/Settings.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Settings = () => {
+  return <div>{/* Settings content goes here */}</div>;
+};
+
+export default Settings;
